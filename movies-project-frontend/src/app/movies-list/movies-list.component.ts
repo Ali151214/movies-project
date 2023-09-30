21947988d40c0d40226382edec8e1f03ed9b5810
ALI ABBAS <aliabbaspanjwani151214@gmail.com>
2023-09-30 16:38:23 +0500

    genre, directors, country filter listing

--- movies-project-frontend/src/app/movies-list/movies-list.component.ts
+++ movies-project-frontend/src/app/movies-list/movies-list.component.ts
@@ -9,12 +9,36 @@ import { HttpCallerService } from "../services/http-caller-service/http-caller.s
 export class MoviesListComponent {
 
   movies = [];
+  genres = [];
+  directors = [];
+  countries = [];
   selectedMovie = {"id": -1};
 
   constructor(private http_caller: HttpCallerService) {}
 
   ngOnInit() {
     this.FetchMovies();
+    this.FetchGenres();
+    this.FetchDirectors();
+    this.FetchCountries();
+  }
+
+  FetchCountries(){
+    this.http_caller.get("countries").subscribe((result: any) => {
+      this.countries = result.data;
+    });
+  }
+
+  FetchDirectors(){
+    this.http_caller.get("directors").subscribe((result: any) => {
+      this.directors = result.data;
+    });
+  }
+
+  FetchGenres(){
+    this.http_caller.get("genres").subscribe((result: any) => {
+      this.genres = result.data;
+    });
   }
 
   FetchMovies(){
